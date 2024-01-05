1687cac4af6d48cd2d5297342f070e1b507efd7e
dhinifia <145754684+dhinifia@users.noreply.github.com>
2024-01-05 16:50:57 +0700

    Update edit_user.php

--- application/views/auth/edit_user.php
+++ application/views/auth/edit_user.php
@@ -7,13 +7,11 @@
     <!-- Sidebar -->
     <?php $this->load->view('back/layouts/_sidebar') ?>
     <!-- End of Sidebar -->
-
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
       <!-- Main Content -->
       <div id="content">
-
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
