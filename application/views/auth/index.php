21af9431f6624b1f361063e760201a48c9f1d87d
dhinifia <145754684+dhinifia@users.noreply.github.com>
2024-01-05 16:52:25 +0700

    Update index.php

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -1,8 +1,6 @@
 <h1><?php echo lang('index_heading');?></h1>
 <p><?php echo lang('index_subheading');?></p>
-
 <div id="infoMessage"><?php echo $message;?></div>
-
 <table cellpadding=0 cellspacing=10>
 	<tr>
 		<th><?php echo lang('index_fname_th');?></th>
