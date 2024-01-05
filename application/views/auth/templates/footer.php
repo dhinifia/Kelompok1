d990dfe7d983354a7ffc7462481e0d7ad0b73e07
dhinifia <145754684+dhinifia@users.noreply.github.com>
2024-01-05 16:57:37 +0700

    Update footer.php

--- application/views/auth/templates/footer.php
+++ application/views/auth/templates/footer.php
@@ -3,8 +3,9 @@
       <script src="<?= base_url("asset/vendor/popper/popper.min.js") ?>"></script>
       <script src="<?= base_url("asset/vendor/bootstrap/js/bootstrap.min.js") ?>"></script>
 
-      <!-- Custom scripts for all pages-->
+	<!-- Custom scripts for all pages-->
       <script src="<?= base_url("asset/js/sb-admin-2.min.js") ?>"></script>
+
 
    </body>
 </html>
